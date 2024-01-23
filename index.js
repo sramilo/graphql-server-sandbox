11185a5ceb56d2bd05cdbd58fdc355b346942375
sramilo <samuelramiloconde@gmail.com>
2024-01-23 21:44:36 +0100

    feat: adds mutation to allow a new person creation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import { v1 as uuid } from 'uuid';
 
 const persons = [
     {
@@ -42,6 +43,15 @@ const typeDefs = gql`
         allPersons: [Person]!
         findPerson(name: String!): Person
     }
+
+    type Mutation {
+        addPerson(
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person!
+    }
 `
 
 const resolvers = {
@@ -51,6 +61,14 @@ const resolvers = {
         findPerson: (root, args) => {
             const { name } = args;
             return persons?.find(p => p.name === name)
+        }
+    },
+    Mutation: {
+        addPerson: (root, args) => {
+            // const { name, phone, street, city } = args;
+            const person = { ...args, id: uuid() }
+            persons.push(person);
+            return person;
         }
     },
     Person: {
